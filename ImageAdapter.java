97c3bf8c0f70ead987e312864a8b0a3b1c0b19cb
HoHyeong Lee <kor_gary@naver.com>
2020-06-19 18:29:05 +0900

    Final OssProject

--- ImageAdapter.java
+++ ImageAdapter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de1af1d74b3dc402564861bc94fc8807c1864cc337e5ed6162c1bd13ff1decc6
-size 1532
+oid sha256:f98ca0710bdc613de39651603d315a75931441f5650416c1f6145f76edf89904
+size 1807
--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8dc9361c377472196b69170cc2b7983b4e1368f2fc64cfad3381b5a53b39562
-size 166950
+oid sha256:554c12594efd0fdbf1c79f6d1e3f35ee2edeebd15063b3ef97478d583d549fcc
+size 168739
